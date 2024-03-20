696ed8979a6f9df7b1832667843e99640332addc
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-03-20 09:13:19 +0700

    kitty fish

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,28 +1,31 @@
-# Helpful aliases
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias un='$aurhelper -Rns' # uninstall package
-alias up='$aurhelper -Syu' # update system/package/aur
-alias pl='$aurhelper -Qs' # list installed package
-alias pa='$aurhelper -Ss' # list availabe package
-alias pc='$aurhelper -Sc' # remove unused cache
-alias po='$aurhelper -Qtdq | $aurhelper -Rns -' # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
-alias vc='code --disable-gpu' # gui code editor
-alias cl=clear
-alias find=fd
-alias cat=bat
-alias hx=helix
-alias top=btop
+if status is-interactive
+    # Helpful aliases
+    alias l='eza -lh  --icons=auto' # long list
+    alias ls='eza -1   --icons=auto' # short list
+    alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+    alias ld='eza -lhD --icons=auto' # long list dirs
+    alias un='$aurhelper -Rns' # uninstall package
+    alias up='$aurhelper -Syu' # update system/package/aur
+    alias pl='$aurhelper -Qs' # list installed package
+    alias pa='$aurhelper -Ss' # list availabe package
+    alias pc='$aurhelper -Sc' # remove unused cache
+    alias po='$aurhelper -Qtdq | $aurhelper -Rns -' # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
+    alias vc='code --disable-gpu' # gui code editor
+    alias cl=clear
+    alias find=fd
+    alias cat=bat
+    alias hx=helix
+    alias top=btop
+    alias :q=exit
 
-# pnpm alias
-alias pn="pnpm"
-alias pni="pnpm install"
-alias pna="pnpm add"
-alias pnad="pnpm add -D"
-alias pnu="pnpm up -Li"
-alias pnd="pnpm run dev"
+    # pnpm alias
+    alias pn="pnpm"
+    alias pni="pnpm install"
+    alias pna="pnpm add"
+    alias pnad="pnpm add -D"
+    alias pnu="pnpm up -Li"
+    alias pnd="pnpm run dev"
 
-# Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
-alias ssh='kitten ssh'
+    # Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
+    alias ssh='kitten ssh'
+end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+# This disables the Fish welcome message
+
+set fish_greeting ""
+
 if status is-interactive
     atuin init fish | source
 end
@@ -9,6 +13,32 @@ set fzf_preview_dir_cmd eza --all --color=always
 
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Little script that only runs if Fish runs interactively and as a login shell. 
+# If you have a display manager like GDM or LightDM, you won't need this
+# You can just press Return to start River. You can also type "Y" or "y" to start it
+# If you don't want to start River, just press Space or any letter
+
+if status is-login && status is-interactive && not pidof -q river
+    echo "- Select a WM"
+    read response -n 1 -P "1 -> Hyprland"\n"2 -> River"\n"3 -> SwayFX"\n"4 -> Wayfire"\n"> "
+    switch $response
+        case 1 ""
+            echo \n"Launching Hyprland..."\n
+            dbus-run-session Hyprland
+        case 2
+            echo \n"Launching River..."\n
+            dbus-run-session river
+        case 3
+            echo \n"Launching SwayFX..."\n
+            dbus-run-session sway
+        case 4
+            echo \n"Launching Wayfire"\n
+            dbus-run-session wayfire
+        case '*'
+            echo \n"No GUI for you..."\n
+    end
+end
 
 # place at the end of file
 zoxide init --cmd cd fish | source
